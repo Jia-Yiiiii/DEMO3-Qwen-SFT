5ca328ef0dd27ce013b7aab3119804bdf9a12471
Jia-Yiiiii <17640079336@163.com>
2026-09-14 13:53:46 +0800

    Update modeldownloead.py

--- modeldownloead.py
+++ modeldownloead.py
@@ -1,9 +1,6 @@
-from huggingface_hub import snapshot_download
-import os
-os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+from modelscope import snapshot_download
 
-snapshot_download(repo_id="Qwen/Qwen2.5-7B",local_dir="./model/Qwen2.5-7B",resume_download=True,
-    ignore_patterns=[
-        ".gitattributes",
-        "README.md",
-    ])
+snapshot_download(
+    'Qwen/Qwen2.5-7B',
+    local_dir='./model/Qwen2.5-7B'
+)
